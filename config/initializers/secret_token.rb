1c4b92f04eddc5e6b1e39a31a53117f24dac48bf
Tim Habermaas <tim.habermaas@gmail.com>
2013-09-21 15:22:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MealsOnWheels::Application.config.secret_key_base = '8284b272fd4ee31a0c4e13c320d74c826d63bacd2af0cf40cb66c2b831f818530978417b47e3ded1267276f5be0188b7e4be1a0926a386d09929c5a68d485254'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MealsOnWheels::Application.config.session_store :cookie_store, key: '_meals_on_wheels_session'
