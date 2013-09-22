4591ce0380c45914d3b16f8edd0dd4982f80efbc
Tim Habermaas <tim.habermaas@gmail.com>
2013-09-22 12:21:19 +0200

    Move navigation items into dedicated NavigationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
-require "ostruct"
-
 module ApplicationHelper
-  def navigation_items
-    [
-      OpenStruct.new(title: "Dashboard", url: root_path, active: true),
-      OpenStruct.new(title: "Kunden", url: root_path),
-      OpenStruct.new(title: "Menus", url: root_path),
-      OpenStruct.new(title: "Touren", url: root_path),
-      OpenStruct.new(title: "Lager", url: root_path)
-    ]
-  end
 end
---
+++ app/helpers/navigation_helper.rb
@@ -0,0 +1,13 @@
+require "ostruct"
+
+module NavigationHelper
+  def navigation_items
+    [
+      OpenStruct.new(title: "Dashboard", url: root_path, active: true),
+      OpenStruct.new(title: "Kunden", url: root_path),
+      OpenStruct.new(title: "Menus", url: root_path),
+      OpenStruct.new(title: "Touren", url: root_path),
+      OpenStruct.new(title: "Lager", url: root_path)
+    ]
+  end
+end
