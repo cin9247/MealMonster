5c82229d91b51eed613393f480f523b2836a771b
Tim Habermaas <tim.habermaas@gmail.com>
2013-09-22 12:20:17 +0200

    Add 'Dashboard' navigation item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ require "ostruct"
 module ApplicationHelper
   def navigation_items
     [
-      OpenStruct.new(title: "Kunden", url: root_path, active: true),
+      OpenStruct.new(title: "Dashboard", url: root_path, active: true),
+      OpenStruct.new(title: "Kunden", url: root_path),
       OpenStruct.new(title: "Menus", url: root_path),
       OpenStruct.new(title: "Touren", url: root_path),
       OpenStruct.new(title: "Lager", url: root_path)
