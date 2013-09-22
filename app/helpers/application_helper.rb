079bd14b0f0f80fad2d1a3d03d9e9d2c11869e8f
Tim Habermaas <tim.habermaas@gmail.com>
2013-09-22 12:17:20 +0200

    Add navigation items; move navigation into partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+require "ostruct"
+
 module ApplicationHelper
+  def navigation_items
+    [
+      OpenStruct.new(title: "Kunden", url: root_path, active: true),
+      OpenStruct.new(title: "Menus", url: root_path),
+      OpenStruct.new(title: "Touren", url: root_path),
+      OpenStruct.new(title: "Lager", url: root_path)
+    ]
+  end
 end
