c1a71f810836ec72337f9a0e889f92155cec5203
Tim Habermaas <tim.habermaas@gmail.com>
2013-10-07 11:13:41 +0200

    Use sequel model class for mapper

--- app/models/mappers/base_mapper.rb
+++ app/models/mappers/base_mapper.rb
@@ -2,28 +2,28 @@ class BaseMapper
   def save(record)
     raise "Can't be saved again. Try #update instead" if record.id
 
-    record.id = DB[table_name].insert hash_from_object(record)
+    record.id = model_class.new(hash_from_object(record)).save[:id]
   end
 
   def update(record)
     raise "Can't update non-existing record. Try #save instead" unless record.id
 
-    DB[table_name].filter(id: record.id)
-                  .update(hash_from_object(record))
+    model_class.where(id: record.id)
+               .update(hash_from_object(record))
   end
 
   def fetch
-    DB[table_name].all.map do |m|
+    model_class.all.map do |m|
       convert_to_object_and_set_id m
     end
   end
 
   def clean
-    DB[table_name].delete
+    model_class.dataset.delete
   end
 
   def find(id)
-    result = DB[table_name].filter(id: id).first
+    result = model_class[id]
 
     convert_to_object_and_set_id(result) if result
   end
@@ -44,6 +44,11 @@ class BaseMapper
     def convert_to_object_and_set_id(hash)
       object_from_hash(hash).tap do |o|
         o.id = hash[:id]
+      end
+    end
+
+    def model_class
+      @model_class ||= Class.new(Sequel::Model(table_name)) do
       end
     end
 end
