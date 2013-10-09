93fb2cae7f04f7bdae101edfb720ae1b83939ec1
Tim Habermaas <tim.habermaas@gmail.com>
2013-10-09 15:25:05 +0200

    Rescue when providing invalid date parameter

--- app/controllers/api/v1/offerings_controller.rb
+++ app/controllers/api/v1/offerings_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::OfferingsController < Api::V1::ApiController
   def index
-    @offerings = organization.day(params[:date]).offerings
+    date = Date.parse params[:date]
+    @offerings = organization.day(date).offerings
+  rescue
+    render json: {errors: [{message: "invalid date"}]}, status: 400
   end
 
   private
--- spec/features/api/offerings_spec.rb
+++ spec/features/api/offerings_spec.rb
@@ -14,30 +14,51 @@ describe "/api/offerings" do
   let(:json_response) { JSON.parse last_response.body }
   let(:result) { json_response["offerings"] }
 
-  describe "GET /api/offerings?date=2013-10-03" do
-    before do
-      kitchen.day("2013-10-03").offer! first_menu
-      kitchen.day("2013-10-03").offer! second_menu
-      kitchen.day("2013-10-04").offer! third_menu
+  before do
+    kitchen.day("2013-10-03").offer! first_menu
+    kitchen.day("2013-10-03").offer! second_menu
+    kitchen.day("2013-10-04").offer! third_menu
+  end
 
-      get "/api/v1/offerings?date=2013-10-03"
+  describe "GET /api/offerings?date=?" do
+    context "given a valid date" do
+
+      before do
+        get "/api/v1/offerings?date=2013-10-03"
+      end
+
+      it "returns 200 OK" do
+        expect(last_response.status).to eq 200
+      end
+
+      it "returns all offerings for that day" do
+        expect(result.length).to eq 2
+        expect(result[0]["date"]).to eq "2013-10-03"
+      end
+
+      it "includes all meals" do
+        expect(result[0]["meals"][0]["name"]).to eq "Spaghetti"
+        expect(result[0]["meals"][1]["name"]).to eq "Pudding"
+        expect(result[1]["meals"][0]["name"]).to eq "Spaghetti"
+        expect(result[1]["meals"][1]["name"]).to eq "Quark"
+        expect(result[1]["meals"][1]["bread_units"]).to eq 2.1
+      end
+
     end
 
-    it "should return 200 OK" do
-      expect(last_response.status).to eq 200
-    end
+    context "given an invalid date" do
+      before do
+        get "/api/v1/offerings?date=2013/441-12"
+      end
 
-    it "returns all offerings for that day" do
-      expect(result.length).to eq 2
-      expect(result[0]["date"]).to eq "2013-10-03"
-    end
+      it "returns 400 Bad Request" do
+        expect(last_response.status).to eq 400
+      end
 
-    it "includes all meals" do
-      expect(result[0]["meals"][0]["name"]).to eq "Spaghetti"
-      expect(result[0]["meals"][1]["name"]).to eq "Pudding"
-      expect(result[1]["meals"][0]["name"]).to eq "Spaghetti"
-      expect(result[1]["meals"][1]["name"]).to eq "Quark"
-      expect(result[1]["meals"][1]["bread_units"]).to eq 2.1
+      it "returns an error mentioning the invalid date" do
+        messages = json_response["errors"].map { |e| e["message"] }
+        expect(messages).to include "invalid date"
+      end
     end
   end
 end
