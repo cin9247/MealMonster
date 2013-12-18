c26b770b0312a79350e31adc0fc21e0477581f47
Tim Habermaas <tim.habermaas@gmail.com>
2013-12-18 10:05:07 +0100

    Move /deliver spec to order

--- spec/features/api/orders_spec.rb
+++ spec/features/api/orders_spec.rb
@@ -54,4 +54,24 @@ describe "/api/offerings" do
       end
     end
   end
+
+  describe "PUT /deliver" do
+    let(:customer) { create_customer }
+
+    before do
+      order = Interactor::CreateOrder.new(customer.id, offering.id).run.object
+      put "/api/v1/orders/#{order.id}/deliver"
+    end
+
+    it "returns status 204 No Content" do
+      expect(last_response.status).to eq 204
+    end
+
+    it "sets the delivered flag to true" do
+      tour = create_tour("Tour", [customer.id])
+      get "api/v1/tours/#{tour.id}?date=2013-10-03"
+
+      expect(json_response["tour"]["stations"][0]["order"]["delivered"]).to eq true
+    end
+  end
 end
