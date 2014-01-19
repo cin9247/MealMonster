04959e6f14243fd9573713f4f09e8af4dc7a098c
Tim Habermaas <tim.habermaas@gmail.com>
2014-01-19 13:53:54 +0100

    Add user's edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @roles = ["admin", "driver", "user"]
+    @user = UserMapper.new.find params[:id]
+  end
+
+  def update
+    user = UserMapper.new.find params[:id]
+    user.name = params[:user][:name]
+    UserMapper.new.update user
+    Interactor::AddRole.new(OpenStruct.new(user_id: user.id, role: params[:user][:role])).run
+
+    redirect_to users_path
+  end
+
   def index
     @users = UserMapper.new.fetch
   end
