4a08c76d186b53c7a169a50995c3310cebc97b73
Tim Habermaas <tim.habermaas@gmail.com>
2014-01-19 20:39:27 +0100

    Remove redundant name column from invoices table

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,6 @@
 class InvoicesController < ApplicationController
   def index
-    @orders = OrderMapper.new.find_by_customer_id(params[:customer_id].to_i)
+    @customer = CustomerMapper.new.find params[:customer_id].to_i
+    @orders = OrderMapper.new.find_by_customer_id(@customer.id)
   end
 end
