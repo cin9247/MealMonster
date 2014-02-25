5cf56f412c90fd3a709f161a7c4a3e3889277cd8
Tim Habermaas <tim.habermaas@gmail.com>
2014-02-25 11:05:42 +0100

    [WIP] Trying to quickfix stuff.

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::TicketsController < Api::V1::ApiController
   respond_to :json
 
   def create
-    request = OpenStruct.new(title: params[:title], body: params[:body], customer_id: params[:customer_id])
+    request = OpenStruct.new(title: params[:title], body: params[:body], customer_id: params[:customer_id].to_i)
     @ticket = InteractorFactory.execute(:create_ticket, request, current_user).object
     respond_with(@ticket, status: 201)
   end
